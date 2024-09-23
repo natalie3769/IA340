af279df0966c1b7f492595539963bd77c6d1ab75
natalie3769 <beanena@dukes.jmu.edu>
2024-09-23 16:26:31 -0400

    lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,1 +1,36 @@
-
+--4.1
+/* no because of the 1 to M relationship and we still have VA records */
+--4.2
+/* no because 80 is not a valid fips */
+--4.3
+/* just fine */
+--4.5
+/* Yes, it is able to recognize the ER diagram from the image, it recognizes the three tables and their realtionships /*
+--4.6
+/* SELECT name.name, MAX(income.income) AS highest_income
+FROM income
+JOIN name ON income.fips = name.fips
+GROUP BY name.name
+ORDER BY highest_income DESC
+LIMIT 1;
+The Query was successful /*
+--4.7
+/* WITH population_5_years_ago AS (
+    SELECT pop, year
+    FROM population
+    JOIN name ON population.fips = name.fips
+    WHERE name.name = 'Virginia' AND year = (SELECT MAX(year) FROM population) - 5
+),
+population_current AS (
+    SELECT pop, year
+    FROM population
+    JOIN name ON population.fips = name.fips
+    WHERE name.name = 'Virginia' AND year = (SELECT MAX(year) FROM population)
+)
+SELECT 
+    ((population_current.pop::numeric / population_5_years_ago.pop::numeric) - 1) * 100 AS growth_rate
+FROM 
+    population_5_years_ago, population_current;
+I modified the prompt to give a more accurate guideline, and the Query was successful /*
+--4.8
+/* It's not always 100% accurate, for SQL code its important to double-check responses
